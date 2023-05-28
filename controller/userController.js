9985f7c1c06206cc5033cd5309e03fbb8e734871
Ankush Kapoor <ankushkapoor179@gmail.com>
2023-05-28 13:13:07 +0530

    Added bcrypt and JWT

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,7 @@
 const User = require("../model/User");
+const bcrypt = require("bcrypt");
+const salt = 10;
+const jwt = require("jsonwebtoken");
 
 // First Syntax
 // const signup = () => {
@@ -23,9 +26,8 @@ const User = require("../model/User");
 // }
 
 exports.signup = async (req, res) => {
-  // We need to check if user is already signedup
-
   try {
+    // We need to check if user is already signed up
     const userExists = await User.findOne({ email: req.body.email });
     console.log("user ---", userExists);
 
@@ -36,12 +38,49 @@ exports.signup = async (req, res) => {
         .json({ message: "User already exists, Try signin" });
     }
 
-    //If not existing
+    // We need to encrypt the user password
+    const encrypedPassword = bcrypt.hashSync(req.body.password, salt);
+    console.log(encrypedPassword);
+
+    // If not existing
     // Then we create the user
-    const createUser = await User.create(req.body);
+    const createUser = await User.create({
+      ...req.body,
+      password: encrypedPassword
+    });
+
     res.status(200).json({ message: "User Created Successfully" });
   } catch (err) {
-    console.log(err);
-    return res.status(500).json({err: err.message})
+    return res.status(500).json({ err: err.message });
+  }
+};
+
+exports.signin = async (req, res) => {
+  try {
+    // req -->  email, password
+    // We need to verify if the user is signedup or not?
+
+    const { email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    const { firstName, _id, role } = userExists;
+    if (!userExists) {
+      return res
+        .status(400)
+        .json({ message: "User doesnot exist, Please Signup first" });
+    }
+
+    // Verify the password
+    const isPasswordCorrect = bcrypt.compareSync(password, userExists.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Password is not correct" });
+    }
+
+    const token = jwt.sign({ firstName, _id, role }, "secret", {
+      expiresIn: "12h"
+    });
+
+    return res.status(200).json({ token, message: "Signin success" });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 // console.log('routes File initialized')
-const { signup } = require("../controller/userController");
+const { signup, signin } = require("../controller/userController");
 
+const validate = (req, res, next) => {
+  console.log('validate called')
+
+  // No errors and we can move to controller
+  // next();
+}
 
 router.post("/signup", signup);
-// router.post("/signin", signin)
+router.post("/signin", signin)
+
+
+router.get('/test', validate ,(req, res) => {
+  console.log('controller called')
+})
 
 
 module.exports = router;
